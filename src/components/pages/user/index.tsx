a7eb78e5542eb33380631f12e61c6ec41c6ac162
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-06-12 21:48:29 +0100

    🔧Fix user page not re-rendering when user signs out

--- src/components/pages/user/index.tsx
+++ src/components/pages/user/index.tsx
@@ -1,19 +1,18 @@
-import { useAuth } from 'reactfire'
+import { useAuth, useSigninCheck } from 'reactfire'
 
 const User = () => {
-  const auth = useAuth()
-  const currentUser = auth.currentUser
+  const { status, data: signInCheckResult } = useSigninCheck()
 
-  return (
-    <div className="flex px-20 py-20">
-      <h1>
-        {' '}
-        {currentUser
-          ? `Hello ${currentUser.email}! 👋`
-          : `No user signed in 😢`}
-      </h1>
-    </div>
-  )
+  if (status === 'loading') {
+    return <h1>Loading...</h1>
+  }
+
+  if (!signInCheckResult.signedIn) {
+    return <h1>No user signed in 😢</h1>
+  }
+
+  const auth = useAuth()
+  return <h1>Hello {auth.currentUser.email}! 👋</h1>
 }
 
 export default User
