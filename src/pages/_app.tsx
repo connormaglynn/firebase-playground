400dc33b17066bf339e7f1c892c304b098b2ffb8
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-06-13 08:02:40 +0100

    ✨Add analytics initialisation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,30 @@
 import '../styles/index.css'
 import { config } from '../config'
-import { AuthProvider, FirebaseAppProvider, useFirebaseApp } from 'reactfire'
+import {
+  AnalyticsProvider,
+  AuthProvider,
+  FirebaseAppProvider,
+  useFirebaseApp,
+} from 'reactfire'
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
-import { StrictMode, useEffect } from 'react'
+import { StrictMode } from 'react'
+import { getAnalytics } from '@firebase/analytics'
 
 const FirebaseProviders = ({ children }) => {
   const app = useFirebaseApp()
+  const analytics = typeof window !== 'undefined' ? getAnalytics(app) : false
   const auth = getAuth(app)
 
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      connectAuthEmulator(auth, 'http://localhost:9099')
-    }
-  }, [auth])
+  if (process.env.NODE_ENV !== 'production') {
+    connectAuthEmulator(auth, 'http://localhost:9099')
+  }
 
+  if (analytics)
+    return (
+      <AnalyticsProvider sdk={analytics}>
+        <AuthProvider sdk={auth}>{children}</AuthProvider>
+      </AnalyticsProvider>
+    )
   return <AuthProvider sdk={auth}>{children}</AuthProvider>
 }
 
